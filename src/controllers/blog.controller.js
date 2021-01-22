6dc662df5090bf4f7df0744572a19620a00d887b
andrusensei <contacttowork.andres+mygithub@gmail.com>
2021-01-22 05:15:57 -0400

    añadidas politicas de freelance26, faltan los terminos y condiciones por terminar

--- src/controllers/blog.controller.js
+++ src/controllers/blog.controller.js
@@ -25,5 +25,42 @@ blogCtrl.renderBlogDetalles = (req, res) => {
     res.render('./blog/detalles-blog')
 };
 
+//Vista términos y condiciones
+blogCtrl.renderTerminosYCondiciones = (req, res) => {
+    if (req.user){
+        if (req.user.tipo_cuenta === 'Empresa'){
+            const empresa = req.user.tipo_cuenta;
+            res.render('./blog/terminos-y-condiciones', {empresa})        
+        }
+
+    }
+    res.render('./blog/terminos-y-condiciones')
+};
+
+//Vista políticas de privacidad
+blogCtrl.renderPoliticaDePrivacidad = (req, res) => {
+    if (req.user){
+        if (req.user.tipo_cuenta === 'Empresa'){
+            const empresa = req.user.tipo_cuenta;
+            res.render('./blog/politica-de-privacidad', {empresa})        
+        }
+
+    }
+    res.render('./blog/politica-de-privacidad')
+};
+
+
+//Vista políticas de freelance26
+blogCtrl.renderPoliticasDeFreelance26 = (req, res) => {
+    if (req.user){
+        if (req.user.tipo_cuenta === 'Empresa'){
+            const empresa = req.user.tipo_cuenta;
+            res.render('./blog/politicas-de-freelance26', {empresa})        
+        }
+
+    }
+    res.render('./blog/politicas-de-freelance26')
+};
+
 //Exportando modulo
 module.exports = blogCtrl;
