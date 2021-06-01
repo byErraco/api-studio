e628153a1ec20fda9f987e94cf7d981a86c5223c
byErraco <51346012+byErraco@users.noreply.github.com>
2021-06-01 10:55:33 -0800

    Update emails.routes.js

--- src/routes/emails.routes.js
+++ src/routes/emails.routes.js
@@ -39,7 +39,7 @@ router.post('/send-mail', async (req,res) => {
     try {
         const info = await transporter.sendMail({
             from: "'Studio73pty Server' <test_web@studio73pty.com>",
-            to: "info@studio73pty.com",
+            to: "studio73pty@gmail.com",
             subject:'Formulario de contacto',
     
             html: contentHTML
